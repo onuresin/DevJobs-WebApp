00fbfc1e01c655fa0f489416227d7ce2aeaa7f15
Onur Esin <onuresn034@gmail.com>
2024-01-06 23:02:23 +0300

    header assetleri, data.json eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,10 @@ import { createBrowserRouter,RouterProvider,} from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,}
+    element: <App />,
+  }
   ])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+
   <RouterProvider router={router} />
 )
