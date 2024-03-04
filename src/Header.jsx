5bee54f7540553ae79a514578f5a5723926d457e
Onur Esin <onuresn034@gmail.com>
2024-03-04 21:05:10 +0300

    header içinde ki image dosyaları import edildi, bir önce ki committe data.json içinde ki görsel yollarınında kök dizini değiştirildi

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
+import lightTheme from '../Public/images/light-group.svg'
+import darkButton from '../Public/images/dark-group.svg'
+import devJobs from '../public/images/devjobs.svg'
 
 export default function Header() {
   const [darkTheme, setDarkTheme] = useState(localStorage.getItem('darkTheme') === 'true' || false);
@@ -16,12 +19,12 @@ export default function Header() {
   return (
     <>
       <div className="navHeader">
-        <Link to="/"><img src="/public/images/devjobs.svg" alt="DevJobs Logo" /></Link>
+        <Link to="/"><img src={devJobs} alt="DevJobs Logo" /></Link>
         <div className="theme-changer" onClick={handleThemeChange}>
           {darkTheme ? 
-            (<img src="/public/images/dark-group.svg" alt="Dark Theme Icon" />) 
+            (<img src={darkButton} alt="Dark Theme Icon" />) 
             : 
-            (<img src="/public/images/light-group.svg" alt="Light Theme Icon" />)
+            (<img src={lightTheme} alt="Light Theme Icon" />)
           }
         </div>
       </div>
